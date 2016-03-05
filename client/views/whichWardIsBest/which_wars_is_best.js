060d099a919dd7cc2124c595d52f7a45e7dfb4c8
Kevin Lee <kevinrobertlee@gmai.com>
2016-03-05 18:08:33 -0500

    FIXED CHRIS'S STUPID CLIENT FUCKUP

--- client/views/whichWardIsBest/which_wars_is_best.js
+++ client/views/whichWardIsBest/which_wars_is_best.js
@@ -1,11 +1,18 @@
+Template.whichWardIsBest.helpers({
+    suggestedWard: function (rank) {
+        return Session.get('suggestedWards')[rank -1].WardNo;
+    }
+});
+
 Template.whichWardIsBest.rendered = function () {
     Meteor.call('compileResults', Session.get('searchParams'), function (err, data) {
         if(err){
             console.log(err);
             return;
         }
-
-
-
+        Session.set('suggestedWards', data);
+        console.log(data);
+        $('#loading').addClass('hidden');
+        $('#ranking').removeClass('hidden');
     })
 };
--- server/methods/misc.js
+++ server/methods/misc.js
@@ -1,18 +1,45 @@
 Meteor.methods({
-    compileResults: function(searchParams){
+    compileResults: function (searchParams) {
         var wards = Wards.find().fetch();
         var wardRanking = setUpWardRanks();
         var wardAgeTotal = setUpWardAgeTotal();
         var wardIncomeTotal = setUpWardIncomeTotal();
 
-        for(var i = 0; i < wards.length; i++){
-            var wardAgeTotal = findWardAgeScore(wards[i].Gender.Male, searchParams.ageRange[0], searchParams.ageRange[1])
-
+        for (var i = 0; i < wards.length; i++) {
+            var wardTotal = findWardAgeTotal(wards[i].Gender.Male, searchParams.ageRange[0], searchParams.ageRange[1]);
+            wardTotal += findWardAgeTotal(wards[i].Gender.Female, searchParams.ageRange[0], searchParams.ageRange[1]);
+            wardAgeTotal[i].wardAgePop = wardTotal;
         }
+        wardAgeTotal.sort(function (a, b) {
+            if (a.wardAgePop > b.wardAgePop) {
+                return 1;
+            }
+            if (a.wardAgePop < b.wardAgePop) {
+                return -1;
+            }
+            return 0;
+        });
+        addAgeScoreToWards(wardRanking, wardAgeTotal);
+
+        for(var i=0; i > wards.length; i++){
+            findTotalPopScore(wards[i], searchParams.totalPopulation, wardRanking);
+        }
+
+        wardRanking.sort(function(a, b){
+            if(a.wardScore > b.wardScore){
+                return 1;
+            }
+            if(a.wardScore < b.wardScore){
+                return -1;
+            }
+            return 0;
+        })
+
+        return wardRanking;
     }
 });
 
-var setUpWardRanks = function(){
+var setUpWardRanks = function () {
     return [
         {
             WardNo: 1,
@@ -159,24 +186,58 @@ var setUpWardIncomeTotal = function () {
     ]
 };
 
-var findWardAgeScore = function (genderData, min, max){
-
-};
-
-var isInRange = function(minRange, maxRange, minVal, maxVal){
-    if(minVal > minRange){
-        if( maxVal > maxRange){
-            return true;
+var findWardAgeTotal = function (genderData, min, max) {
+    var ageTotal = 0;
+    for (var i = 0; i < genderData.length; i++) {
+        if (min > genderData[i].min && max > genderData[i].max) {
+            ageTotal += genderData[i].value;
         }
     }
-    return false;
+    return ageTotal;
 };
 
+var addAgeScoreToWards = function (wardRanking, wardAgeTotal) {
+    var score = 3;
+    var iterations = 0;
+
+    for (var i = 0; i < wardAgeTotal.length; i++) {
+        var wardNo = wardAgeTotal[i].WardNo;
+        wardRanking.filter(function (ward) {
+            if (ward.WardNo == wardNo) {
+                ward.wardScore += score;
+            }
+        });
+        if (iterations == 0 || iterations == 2 || iterations == 4 || iterations == 5 || iterations == 7 || iterations == 9) {
+            score -= 1;
+        }
+        iterations++;
+    }
+};
 
 var findGenderScore = function () {
 
 };
 
-var findTotalPopScore = function () {
+var findTotalPopScore = function (ward, population, rankings) {
+    var delta = ward.TotalPopulation - population;
+    var score = 0;
+    if(delta < -540){
+        score = -3
+    }else if (delta >= -539 && delta <= -360){
+        score = -2;
+    }else if (delta >= 359 && delta <= -180 ){
+        score = 0;
+    }else if(delta >= -179 && delta <= 180){
+        score = 1;
+    }else if(delta > 181 && delta <= 360){
+        score = 3;
+    }else if (delta > 360){
+        score = 3;
+    }
 
+    rankings.filter(function (obj) {
+        if(obj.WardNo == ward.WardNo){
+            obj.wardScore += score;
+        }
+    });
 };
