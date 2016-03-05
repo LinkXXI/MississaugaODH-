e0e27903efb0ad733a3f0eaf2d092c2226d3d409
Kevin Lee <kevinrobertlee@gmai.com>
2016-03-05 15:24:47 -0500

    Working on search front end

--- client/views/whereToGo/whereToGo.js
+++ client/views/whereToGo/whereToGo.js
@@ -1,12 +1,26 @@
-Template.whereToGo.rendered = function () {
-    var slider = document.getElementById('slider');
+Template.whereToGo.events({
+    "form submit": function (e, template){
+        e.preventDefault();
 
-    noUiSlider.create(slider, {
-        start: [0, 5],
-        connect: true,
-        range: {
-            'min': 0,
-            'max': 85
-        }
-    })
+        var searchParams = {};
+
+        var minAge = parseInt($('#fromAge').val());
+        var maxAge = parseInt($('#toAge').val());
+        searchParams.ageRange = [minAge, maxAge];
+
+        var minIncome = parseInt($('#fromIncome').val());
+        var maxIncome = parseInt($('#toIncome').val());
+        searchParams.incomeRange = [minIncome, maxIncome];
+
+        searchParams.preferedGender = $('input[type=radio][name=gender]:checked').attr('id');
+        searchParams.genderPercent = parseInt($('#genderPercent').val());
+
+        searchParams.totalPopulation = $('#totalPopulation').val();
+
+        Session.set('searchParams', searchParams);
+        Router.go('/whichWardIsBest');
+    }
+});
+
+Template.whereToGo.rendered = function () {
 };
